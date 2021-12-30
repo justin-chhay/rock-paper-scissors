8da1efa5d001f25f0d80ea96d5f91aaae1210a0f
justin-chhay <justinschhay@gmail.com>
2021-12-29 20:59:06 -0500

    created game logic and UI for 5 rounds

--- game.js
+++ game.js
@@ -8,6 +8,13 @@
 const choices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection = "";
+let computerSelection = "";
+
+//DOM Variables
+let buttons = document.querySelectorAll(".button");
+const roundResult = document.querySelector("#results-container");
+
 
 //Randomly generates computer decision
 function computerPlay() {
@@ -17,41 +24,42 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
 
     //loop through turn in case user misspells
-    while (true) {
-        let playerAns = choices.indexOf(playerSelection.toLowerCase());
-        let computerAns = choices.indexOf(computerSelection.toLowerCase());
+    let playerAns = choices.indexOf(playerSelection.toLowerCase());
+    let computerAns = choices.indexOf(computerSelection.toLowerCase());
 
-        if (playerAns === -1) {
-            console.log("Please recheck the spelling of your choice.");
-            playerSelection = prompt("Choose rock, paper, or scissors");
-            continue;
-        }
-
-        if (playerAns === computerAns) { //draw
-            return "You both chose " + playerSelection + ". DRAW";
-        } else if (
-            (playerAns === 0 && computerAns === 2) ||
-            (playerAns === 2 && computerAns === 1) ||
-            (playerAns === 1 && computerAns === 0)
-        ) {
-            ++playerScore;
-            return playerSelection.substr(0, 1).toUpperCase() + playerSelection.substr(1).toLowerCase() + " beats " + computerSelection + ". WINNER";
-        } else { //lose
-            ++computerScore;
-            return playerSelection.substr(0, 1).toUpperCase() + playerSelection.substr(1).toLowerCase() + " loses against " + computerSelection + ". LOSS";
-        }
-        break;
+    if (playerAns === computerAns) { //draw
+        displayResult("You both chose " + playerSelection + ". DRAW");
+    } else if (
+        (playerAns === 0 && computerAns === 2) ||
+        (playerAns === 2 && computerAns === 1) ||
+        (playerAns === 1 && computerAns === 0)
+    ) {
+        ++playerScore;
+        displayResult(playerSelection.substr(0, 1).toUpperCase() + playerSelection.substr(1).toLowerCase() + " beats " + computerSelection + ". WINNER");
+    } else { //lose
+        ++computerScore;
+        displayResult(playerSelection.substr(0, 1).toUpperCase() + playerSelection.substr(1).toLowerCase() + " loses against " + computerSelection + ". LOSS");
     }
 }
 
-function game() {
-    //PLay 5 Rounds
-    for (let i = 0; i < 5; ++i) {
-        let playerSelection = prompt("Choose rock, paper, or scissors");
-        let computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-    }
 
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        computerSelection = computerPlay();
+        playerSelection = button.id;
+        console.log(playerSelection);
+        playRound(playerSelection, computerSelection);
+        if ((playerScore + computerScore) === 5) {
+            endGame();
+        }
+    });
+});
+
+function displayResult(result) {
+    roundResult.textContent = result;
+}
+
+function endGame() {
     //Game results
     console.log("player score = " + playerScore);
     console.log("computer score = " + computerScore);
@@ -62,8 +70,3 @@ function game() {
     else
         return alert("YOU WON");
 }
-
-
-//Run game
-console.log("Hello, World!");
-game();
